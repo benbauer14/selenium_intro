8b5a3c997e8e9eff2cd008efb8fbfefc1cc1b196
Ben Bauer <ben.bauer1@gmail.com>
2021-07-31 17:04:37 -0500

    scrapes data

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ chrome_driver_path = "/Users/benbauer/Documents/VSCode/chromedriver"
 
 driver = webdriver.Chrome(chrome_driver_path)
 
-driver.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
-
+driver.get("https://www.bestbuy.com/site/msi-gf65-15-6-144hz-gaming-laptop-intel-core-i5-nvidia-geforce-rtx3060-512gb-ssd-8gb-memory-black/6456144.p?skuId=6456144")
+stock = driver.find_element_by_class_name("fulfillment-fulfillment-summary")
+print(stock.text)
 driver.quit()
 
