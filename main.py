b0a6d901440859f3bb3fa2309f2aa3b19d5ebe2b
Ben Bauer <ben.bauer1@gmail.com>
2021-07-31 16:50:47 -0500

    opens page and closes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
-chrome_driver_path = "/Users/benbauer/Documents/VSCode"
+chrome_driver_path = "/Users/benbauer/Documents/VSCode/chromedriver"
 
 driver = webdriver.Chrome(chrome_driver_path)
 
+driver.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
 
+driver.quit()
 
